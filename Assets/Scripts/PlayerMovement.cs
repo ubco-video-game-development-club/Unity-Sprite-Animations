acca3e54143095a598e4b4642c87cb4eb4fa39eb
Megan Nguyen <megan.nguyenn261@gmail.com>
2021-03-05 01:11:24 -0800

    Create base project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    public float speed = 1f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        float dx = 0, dy = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            dx--;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dx++;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            dy++;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dy--;
+        }
+
+        animator.SetFloat("Vertical", dy);
+        animator.SetFloat("Horizontal", dx);
+
+        Vector3 direction = new Vector3(dx, dy).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
+
+
